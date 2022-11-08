957df9969130d4fb5048832b47b0ce692a731b74
Anthony Lee <anthony8lee@gmail.com>
2022-11-08 14:55:14 -0700

    Two typo corrections.

--- tutorials/W2D3_BiologicalNeuronModels/solutions/W2D3_Tutorial1_Solution_c6f1c4a2.py
+++ tutorials/W2D3_BiologicalNeuronModels/solutions/W2D3_Tutorial1_Solution_c6f1c4a2.py
@@ -8,12 +8,12 @@ non-linearities and render a neuron as a linear system.
 2. (here is a short answer) When we increase the mean of the GWN, at some point
 effective input mean is above the spike threshold and then the neuron operates
 in the so called mean-driven regime -- as the input is so high all the neuron
-is does is charge up to the spike threshold and reset. This essentially gives
+does is charge up to the spike threshold and reset. This essentially gives
 almost regular spiking.
 
 3. In an LIF, high firing rates are achieved for high GWN mean. Higher the mean,
 higher the firing rate and lower the CV_ISI. So you will expect that as firing rate
 increases, spike irregularity decreases. This is because of the spike threshold.
-FOr a Poisson process there is no relationship between spike rate and spike
+For a Poisson process there is no relationship between spike rate and spike
 irregularity.
 """;
